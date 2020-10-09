cacb5961c2f9ca7d19f60286a9fe1f6c3aa22ebf
TASS <48039235+tasszz2k@users.noreply.github.com>
2020-10-09 15:50:20 +0700

    add update quantity

--- MyFilmManagement/Program.cs
+++ MyFilmManagement/Program.cs
@@ -6,6 +6,7 @@ namespace MyFilmManagement
     {
         static void Main(string[] args)
         {
+   
             Console.WriteLine("Hello World!");
             CFilm[] listCFilms = new CFilm[3];
             ManagementFilm m = new ManagementFilm(listCFilms);
